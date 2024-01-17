61dcfa87c73aad3026d4da49db2db68b95ae0078
DenisGladkih <denis.gladkih.55@yandex.ru>
2024-01-18 00:26:27 +0600

    add onboarding navigation destinations

---
+++ pos/app/src/main/java/net/nomia/pos/ui/onboarding/navigation/components/NavigationDirections.kt
@@ -0,0 +1,33 @@
+package net.nomia.pos.ui.onboarding.navigation.components
+
+import com.compose.type_safe_args.annotation.ComposeDestination
+
+@ComposeDestination
+interface OnboardingFirstStepDestination {
+    companion object
+}
+
+@ComposeDestination
+interface OnboardingSecondStepDestination {
+    companion object
+}
+
+@ComposeDestination
+interface OnboardingThirdStepDestination {
+    companion object
+}
+
+@ComposeDestination
+interface OnboardingFourthStepDestination {
+    companion object
+}
+
+@ComposeDestination
+interface OnboardingFifthStepDestination {
+    companion object
+}
+
+@ComposeDestination
+interface OnboardingSixthStepDestination {
+    companion object
+}
