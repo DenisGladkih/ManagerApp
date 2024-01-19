bac1ad6e0da7a4be716b04bb38530da6ca396ab2
DenisGladkih <denis.gladkih.55@yandex.ru>
2024-01-19 17:26:17 +0600

    update modifier

--- pos/app/src/main/java/net/nomia/pos/ui/onboarding/components/Modifiers.kt
+++ pos/app/src/main/java/net/nomia/pos/ui/onboarding/components/Modifiers.kt
@@ -2,14 +2,22 @@ package net.nomia.pos.ui.onboarding.components
 
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.width
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.composed
+import androidx.compose.ui.res.booleanResource
 import androidx.compose.ui.unit.dp
+import net.nomia.pos.R
 
-internal fun Modifier.onboardingContentWidth(isTablet: Boolean): Modifier {
-    val tabletContentWidth = 560.dp
-    return if (isTablet) {
-        this.width(width = tabletContentWidth)
-    } else {
-        this.fillMaxWidth()
+internal fun Modifier.onboardingContentWidth(): Modifier = composed {
+    val isTablet = booleanResource(id = R.bool.isTablet)
+    val modifier = remember {
+        if (isTablet) {
+            val tabletContentWidth = 560.dp
+            this.width(width = tabletContentWidth)
+        } else {
+            this.fillMaxWidth()
+        }
     }
+    modifier
 }
