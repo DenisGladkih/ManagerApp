6da936bf5c3ba40c524420c1cd22e7cb96bbd6c3
DenisGladkih <denis.gladkih.55@yandex.ru>
2024-01-24 11:47:31 +0600

    delete unused params OnboardingMviFeatureFactory

--- pos/app/src/main/java/net/nomia/pos/ui/onboarding/mvi/OnboardingMviFeatureFactory.kt
+++ pos/app/src/main/java/net/nomia/pos/ui/onboarding/mvi/OnboardingMviFeatureFactory.kt
@@ -4,13 +4,10 @@ import net.nomia.mvi.Event
 import net.nomia.mvi.MviFeatureFactory
 
 internal class OnboardingMviFeatureFactory(
-//    bootstrap: OnboardingMviBootstrap,
     actor: OnboardingMviActor,
 ) : MviFeatureFactory<OnboardingMviAction, OnboardingMviEffect, OnboardingMviState, Event>(
     initialState = OnboardingMviState.INITIAL,
-//    bootstrap = bootstrap,
     actor = actor,
-    eventProducer = OnboardingMviEventProducer,
     reducer = OnboardingMviReducer,
     tagPostfix = "OnboardingMviFeature"
 )
