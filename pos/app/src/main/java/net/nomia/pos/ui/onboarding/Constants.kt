d209a660ce5f4dae296c2550bb8b37e7ee8e4ebf
DenisGladkih <denis.gladkih.55@yandex.ru>
2024-01-24 11:32:53 +0600

    add Constants.kt

---
+++ pos/app/src/main/java/net/nomia/pos/ui/onboarding/Constants.kt
@@ -0,0 +1,27 @@
+package net.nomia.pos.ui.onboarding
+
+internal const val CLIENT_NAME = "CLIENT_NAME"
+internal const val PHONE_OR_EMAIL = "PHONE_OR_EMAIL"
+
+internal const val NAME_OF_SHOP = "NAME_OF_SHOP"
+internal const val COUNTRY_AND_CITY = "COUNTRY_AND_CITY"
+internal const val ADDRESS = "ADDRESS"
+internal const val IS_NEW_PLACE = "IS_NEW_PLACE"
+internal const val AUTOMATION_SYSTEM = "AUTOMATION_SYSTEM"
+
+internal const val IS_RESTAURANT = "IS_RESTAURANT"
+internal const val IS_BAR = "IS_BAR"
+internal const val IS_CAFE = "IS_CAFE"
+internal const val IS_DINING_ROOM = "IS_DINING_ROOM"
+internal const val IS_COFFEE_HOUSE = "IS_COFFEE_HOUSE"
+internal const val IS_COOKING = "IS_COOKING"
+internal const val IS_OTHER = "IS_OTHER"
+
+internal const val TOTAL_AREA = "TOTAL_AREA"
+internal const val NUMBER_OF_SEATS = "NUMBER_OF_SEATS"
+internal const val PUBLIC_AREA = "PUBLIC_AREA"
+internal const val KITCHEN_AREA = "KITCHEN_AREA"
+
+internal const val IS_TAKE_AWAY = "IS_TAKE_AWAY"
+internal const val IS_IN_THE_STORE = "IS_IN_THE_STORE"
+internal const val IS_DELIVERY = "IS_DELIVERY"
