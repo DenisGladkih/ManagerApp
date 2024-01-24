4036571f0af820a9f66475dc53e7bb341e2711dd
DenisGladkih <denis.gladkih.55@yandex.ru>
2024-01-24 11:49:53 +0600

    update OnboardingUseCaseModule

--- pos/app/src/main/java/net/nomia/pos/ui/onboarding/di/OnboardingUseCaseModule.kt
+++ pos/app/src/main/java/net/nomia/pos/ui/onboarding/di/OnboardingUseCaseModule.kt
@@ -4,59 +4,77 @@ import dagger.Binds
 import dagger.Module
 import dagger.hilt.InstallIn
 import dagger.hilt.android.components.ViewModelComponent
-//import net.nomia.pos.ui.auth.pos_setup.domain.AuthUseCase
-//import net.nomia.pos.ui.auth.pos_setup.domain.AuthUseCaseImpl
-//import net.nomia.pos.ui.auth.pos_setup.domain.MenusUseCase
-//import net.nomia.pos.ui.auth.pos_setup.domain.MenusUseCaseImpl
-//import net.nomia.pos.ui.auth.pos_setup.domain.OrganizationsUseCase
-//import net.nomia.pos.ui.auth.pos_setup.domain.OrganizationsUseCaseImpl
-//import net.nomia.pos.ui.auth.pos_setup.domain.RestoreTerminalUseCase
-//import net.nomia.pos.ui.auth.pos_setup.domain.RestoreTerminalUseCaseImpl
-//import net.nomia.pos.ui.auth.pos_setup.domain.SaveTerminalUseCase
-//import net.nomia.pos.ui.auth.pos_setup.domain.SaveTerminalUseCaseImpl
-//import net.nomia.pos.ui.auth.pos_setup.domain.ServerProviderUseCase
-//import net.nomia.pos.ui.auth.pos_setup.domain.ServerProviderUseCaseImpl
-//import net.nomia.pos.ui.auth.pos_setup.domain.StoresUseCase
-//import net.nomia.pos.ui.auth.pos_setup.domain.StoresUseCaseImpl
+import net.nomia.pos.ui.onboarding.domain.usecase.GetFifthStepValueUseCase
+import net.nomia.pos.ui.onboarding.domain.usecase.GetFifthStepValueUseCaseImpl
+import net.nomia.pos.ui.onboarding.domain.usecase.GetFirstStepValueUseCase
+import net.nomia.pos.ui.onboarding.domain.usecase.GetFirstStepValueUseCaseImpl
+import net.nomia.pos.ui.onboarding.domain.usecase.GetFourthStepValueUseCase
+import net.nomia.pos.ui.onboarding.domain.usecase.GetFourthStepValueUseCaseImpl
+import net.nomia.pos.ui.onboarding.domain.usecase.GetSecondStepValueUseCase
+import net.nomia.pos.ui.onboarding.domain.usecase.GetSecondStepValueUseCaseImpl
+import net.nomia.pos.ui.onboarding.domain.usecase.GetThirdStepValueUseCase
+import net.nomia.pos.ui.onboarding.domain.usecase.GetThirdStepValueUseCaseImpl
+import net.nomia.pos.ui.onboarding.domain.usecase.SaveFifthStepValueUseCase
+import net.nomia.pos.ui.onboarding.domain.usecase.SaveFifthStepValueUseCaseImpl
+import net.nomia.pos.ui.onboarding.domain.usecase.SaveFirstStepValueUseCase
+import net.nomia.pos.ui.onboarding.domain.usecase.SaveFirstStepValueUseCaseImpl
+import net.nomia.pos.ui.onboarding.domain.usecase.SaveFourthStepValueUseCase
+import net.nomia.pos.ui.onboarding.domain.usecase.SaveFourthStepValueUseCaseImpl
+import net.nomia.pos.ui.onboarding.domain.usecase.SaveSecondStepValueUseCase
+import net.nomia.pos.ui.onboarding.domain.usecase.SaveSecondStepValueUseCaseImpl
+import net.nomia.pos.ui.onboarding.domain.usecase.SaveThirdStepValueUseCase
+import net.nomia.pos.ui.onboarding.domain.usecase.SaveThirdStepValueUseCaseImpl
 
 @Module
 @InstallIn(ViewModelComponent::class)
-interface OnboardingUseCaseModule {
-//
-//    @Binds
-//    fun bindAuthUseCase(
-//        authUseCase: LogoutUseCaseImpl,
-//    ): LogoutUseCase
-//
-//    @Binds
-//    fun bindOrganizationsUseCase(
-//        organizationsUseCase: OrganizationsUseCaseImpl,
-//    ): OrganizationsUseCase
-//
-//    @Binds
-//    fun bindStoresUseCase(
-//        storesUseCase: StoresUseCaseImpl,
-//    ): StoresUseCase
-//
-//    @Binds
-//    fun bindMenusUseCase(
-//        menusUseCase: MenusUseCaseImpl,
-//    ): MenusUseCase
-//
-//    @Binds
-//    fun bindSaveTerminalUseCase(
-//        saveTerminalUseCase: SaveTerminalUseCaseImpl,
-//    ): SaveTerminalUseCase
-//
-//    @Binds
-//    fun bindRestoreTerminalUseCase(
-//        restoreTerminalUseCase: RestoreTerminalUseCaseImpl,
-//    ): RestoreTerminalUseCase
-//
-//    @Binds
-//    fun bindServerProviderUseCase(
-//        serverProviderUseCase: ServerProviderUseCaseImpl,
-//    ): ServerProviderUseCase
+internal interface OnboardingUseCaseModule {
+    @Binds
+    fun bindGetFirstStepValueUseCase(
+        getFirstStepValueUseCase: GetFirstStepValueUseCaseImpl,
+    ): GetFirstStepValueUseCase
 
+    @Binds
+    fun bindSaveFirstStepValueUseCaseImpl(
+        saveFirstStepValueImpl: SaveFirstStepValueUseCaseImpl,
+    ): SaveFirstStepValueUseCase
+
+    @Binds
+    fun bindGetSecondStepValueUseCase(
+        getSecondStepValueUseCaseImpl: GetSecondStepValueUseCaseImpl,
+    ): GetSecondStepValueUseCase
+
+    @Binds
+    fun bindSaveSecondStepValueUseCase(
+        saveSecondStepValueUseCaseImpl: SaveSecondStepValueUseCaseImpl,
+    ): SaveSecondStepValueUseCase
+
+    @Binds
+    fun bindGetThirdStepValueUseCase(
+        getThirdStepValueUseCase: GetThirdStepValueUseCaseImpl,
+    ): GetThirdStepValueUseCase
+
+    @Binds
+    fun bindSaveThirdStepValueUseCase(
+        saveThirdStepValueUseCase: SaveThirdStepValueUseCaseImpl,
+    ): SaveThirdStepValueUseCase
+
+    @Binds
+    fun bindGetFourthStepValueUseCase(
+        getFourthStepValueUseCase: GetFourthStepValueUseCaseImpl,
+    ): GetFourthStepValueUseCase
+
+    @Binds
+    fun bindSaveFourthStepValueUseCase(
+        saveFourthStepValueUseCase: SaveFourthStepValueUseCaseImpl,
+    ): SaveFourthStepValueUseCase
+
+    @Binds
+    fun bindGetFifthStepValueUseCase(
+        getFifthStepValueUseCase: GetFifthStepValueUseCaseImpl,
+    ): GetFifthStepValueUseCase
+
+    @Binds
+    fun bindSaveFifthStepValueUseCase(
+        saveFifthStepValueUseCase: SaveFifthStepValueUseCaseImpl,
+    ): SaveFifthStepValueUseCase
 }
-
