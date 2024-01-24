13aeed9d8c6f1827c93853f8c99337759976b373
DenisGladkih <denis.gladkih.55@yandex.ru>
2024-01-24 11:48:41 +0600

    update OnboardingSecondStep

--- pos/app/src/main/java/net/nomia/pos/ui/onboarding/components/OnboardingSecondStep.kt
+++ pos/app/src/main/java/net/nomia/pos/ui/onboarding/components/OnboardingSecondStep.kt
@@ -1,10 +1,11 @@
-package net.nomia.pos.ui.onboarding.composable
+package net.nomia.pos.ui.onboarding.components
 
 import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -14,13 +15,20 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.TextRange
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.TextFieldValue
 import net.nomia.common.ui.composable.NomiaOutlinedTextField
 import net.nomia.common.ui.composable.list_item.NomiaCheckboxListItem
 import net.nomia.common.ui.theme.spacers
 import net.nomia.pos.R
+import net.nomia.pos.ui.onboarding.model.SecondStepValue
+import net.nomia.pos.ui.onboarding.mvi.OnboardingMviAction
 
 @Composable
 internal fun OnboardingSecondStep(
+    state: SecondStepValue,
+    onAction: (OnboardingMviAction) -> Unit,
 ) {
     Column {
         Text(
@@ -39,56 +47,69 @@ internal fun OnboardingSecondStep(
 
         Spacer(modifier = Modifier.height(height = MaterialTheme.spacers.large))
 
+        var nameOfShop by remember(state.nameOfShop) {
+            mutableStateOf(
+                value = TextFieldValue(
+                    text = state.nameOfShop,
+                    selection = TextRange(state.nameOfShop.length)
+                ),
+            )
+        }
+
         NomiaOutlinedTextField(
-            value = "",
-            onValueChange = {},
+            value = nameOfShop,
+            onValueChange = { newValue ->
+                nameOfShop = newValue
+                onAction(OnboardingMviAction.InputNameOfShop(nameOfShop = newValue.text))
+            },
             placeholder = {
-                Text(
-                    text = stringResource(id = R.string.name_of_place),
-                )
+                Text(text = stringResource(id = R.string.name_of_shop))
             },
             supportingText = {
-                Text(
-                    text = stringResource(id = R.string.then_you_can),
-                )
+                Text(text = stringResource(id = R.string.then_you_can))
             },
+            keyboardOptions = KeyboardOptions(imeAction = ImeAction.Next),
+            showKeyboard = true,
             modifier = Modifier.fillMaxWidth(),
         )
 
         Spacer(modifier = Modifier.height(height = MaterialTheme.spacers.medium))
 
+        var countryAndCity by remember(state.countryAndCity) { mutableStateOf(value = state.countryAndCity) }
+
         NomiaOutlinedTextField(
-            value = "",
-            onValueChange = {},
+            value = countryAndCity,
+            onValueChange = { newValue ->
+                countryAndCity = newValue
+                onAction(OnboardingMviAction.InputCountryAndCity(countryAndCity = newValue))
+            },
             placeholder = {
-                Text(
-                    text = stringResource(id = R.string.country_and_city),
-                )
+                Text(text = stringResource(id = R.string.country_and_city))
             },
             supportingText = {
-                Text(
-                    text = stringResource(id = R.string.required_to_determine_the_time_zone),
-                )
+                Text(text = stringResource(id = R.string.required_to_determine_the_time_zone))
             },
+            keyboardOptions = KeyboardOptions(imeAction = ImeAction.Next),
             modifier = Modifier.fillMaxWidth(),
         )
 
         Spacer(modifier = Modifier.height(height = MaterialTheme.spacers.medium))
 
+        var address by remember(state.address) { mutableStateOf(value = state.address) }
+
         NomiaOutlinedTextField(
-            value = "",
-            onValueChange = {},
+            value = address,
+            onValueChange = { newValue ->
+                address = newValue
+                onAction(OnboardingMviAction.InputAddress(address = newValue))
+            },
             placeholder = {
-                Text(
-                    text = stringResource(id = R.string.address),
-                )
+                Text(text = stringResource(id = R.string.address))
             },
             modifier = Modifier.fillMaxWidth(),
         )
 
         Spacer(modifier = Modifier.height(height = MaterialTheme.spacers.extraSmall + MaterialTheme.spacers.small))
-
-        var checked by remember { mutableStateOf(value = true) }
 
         NomiaCheckboxListItem(
             headlineText = {
@@ -101,27 +122,28 @@ internal fun OnboardingSecondStep(
                     text = stringResource(id = R.string.check_the_box),
                 )
             },
-            onClick = {
-                checked = !checked
+            onClick = { newValue ->
+                onAction(OnboardingMviAction.SetIsNewPlace(isNewPlace = newValue))
             },
-            checked = checked,
+            checked = state.isNewPlace,
         )
 
         Spacer(modifier = Modifier.height(height = MaterialTheme.spacers.extraSmall + MaterialTheme.spacers.small))
 
-        AnimatedVisibility(visible = !checked) {
+        AnimatedVisibility(visible = !state.isNewPlace) {
+            var automationSystem by remember(state.automationSystem) { mutableStateOf(value = state.automationSystem) }
+
             NomiaOutlinedTextField(
-                value = "",
-                onValueChange = {},
+                value = automationSystem,
+                onValueChange = { newValue ->
+                    automationSystem = newValue
+                    onAction(OnboardingMviAction.InputAutomationSystem(automationSystem = newValue))
+                },
                 placeholder = {
-                    Text(
-                        text = stringResource(id = R.string.automation_system),
-                    )
+                    Text(text = stringResource(id = R.string.automation_system))
                 },
                 supportingText = {
-                    Text(
-                        text = stringResource(id = R.string.name_of_the_system),
-                    )
+                    Text(text = stringResource(id = R.string.name_of_the_system))
                 },
                 modifier = Modifier.fillMaxWidth(),
             )
