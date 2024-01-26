470058147882fa3bd8c563531f6521484cb84954
DenisGladkih <denis.gladkih.55@yandex.ru>
2024-01-27 01:10:17 +0600

    add val position

--- pos/app/src/main/java/net/nomia/pos/ui/onboarding/model/OnboardingStep.kt
+++ pos/app/src/main/java/net/nomia/pos/ui/onboarding/model/OnboardingStep.kt
@@ -1,10 +1,10 @@
 package net.nomia.pos.ui.onboarding.model
 
-internal enum class OnboardingStep {
-    FIRST,
-    SECOND,
-    THIRD,
-    FOURTH,
-    FIFTH,
-    SIXTH,
+internal enum class OnboardingStep(val position: Int) {
+    FIRST(position = 0),
+    SECOND(position = 1),
+    THIRD(position = 2),
+    FOURTH(position = 3),
+    FIFTH(position = 4),
+    SIXTH(position = 5),
 }
