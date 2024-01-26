0129a1664f997a446166cc2055376594f500ec7a
DenisGladkih <denis.gladkih.55@yandex.ru>
2024-01-27 01:10:56 +0600

    OnboardingSegmentsBar refactoring

--- pos/app/src/main/java/net/nomia/pos/ui/onboarding/components/OnboardingSegmentsBar.kt
+++ pos/app/src/main/java/net/nomia/pos/ui/onboarding/components/OnboardingSegmentsBar.kt
@@ -10,25 +10,23 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import net.nomia.common.ui.theme.spacers
-import net.nomia.pos.ui.onboarding.model.OnboardingStep
 
 @Composable
 internal fun OnboardingSegmentsBar(
-    onboardingStep: OnboardingStep,
+    segmentsCount: Int,
+    coloredSegments: Int,
 ) = Row(
     modifier = Modifier.fillMaxWidth(),
     horizontalArrangement = Arrangement.spacedBy(space = MaterialTheme.spacers.extraSmall)
 ) {
-    val onboardingStepList = remember { OnboardingStep.values() }
-    onboardingStepList.forEach { step ->
+    repeat(times = segmentsCount) { segment ->
         val animatedColor by animateColorAsState(
-            if (step.ordinal >= onboardingStep.ordinal + 1) {
-                MaterialTheme.colorScheme.surfaceVariant
-            } else {
+            if (coloredSegments > segment) {
                 MaterialTheme.colorScheme.primary
+            } else {
+                MaterialTheme.colorScheme.surfaceVariant
             },
             label = "color"
         )
