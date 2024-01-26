8019653afb3bc31a97135a0f0a8ca412f1ee9588
DenisGladkih <denis.gladkih.55@yandex.ru>
2024-01-27 01:17:32 +0600

    add ManagerData.kt

---
+++ pos/app/src/main/java/net/nomia/pos/ui/onboarding/model/ManagerData.kt
@@ -0,0 +1,9 @@
+package net.nomia.pos.ui.onboarding.model
+
+internal data class ManagerData(
+    val firstStepValue: FirstStepValue,
+    val secondStepValue: SecondStepValue,
+    val thirdStepValue: ThirdStepValue,
+    val fourthStepValue: FourthStepValue,
+    val fifthStepValue: FifthStepValue,
+)
