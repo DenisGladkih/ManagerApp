67a40682aeab9b93d0a86a6b2a2b3d832bb83f7b
DenisGladkih <denis.gladkih.55@yandex.ru>
2024-01-27 01:16:15 +0600

    add saveManagerDataUseCase

--- pos/app/src/main/java/net/nomia/pos/ui/onboarding/di/OnboardingModule.kt
+++ pos/app/src/main/java/net/nomia/pos/ui/onboarding/di/OnboardingModule.kt
@@ -1,10 +1,14 @@
 package net.nomia.pos.ui.onboarding.di
 
+import dagger.Binds
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.components.ViewModelComponent
+import dagger.hilt.components.SingletonComponent
 import net.nomia.auth.domain.LogoutUseCase
+import net.nomia.pos.ui.onboarding.data.OnboardingRepository
+import net.nomia.pos.ui.onboarding.domain.OnboardingRepositoryImpl
 import net.nomia.pos.ui.onboarding.domain.usecase.GetFifthStepValueUseCase
 import net.nomia.pos.ui.onboarding.domain.usecase.GetFirstStepValueUseCase
 import net.nomia.pos.ui.onboarding.domain.usecase.GetFourthStepValueUseCase
@@ -15,8 +19,10 @@ import net.nomia.pos.ui.onboarding.domain.usecase.SaveFirstStepValueUseCase
 import net.nomia.pos.ui.onboarding.domain.usecase.SaveFourthStepValueUseCase
 import net.nomia.pos.ui.onboarding.domain.usecase.SaveSecondStepValueUseCase
 import net.nomia.pos.ui.onboarding.domain.usecase.SaveThirdStepValueUseCase
+import net.nomia.pos.ui.onboarding.domain.usecase.SaveManagerDataUseCase
 import net.nomia.pos.ui.onboarding.mvi.OnboardingMviActor
 import net.nomia.pos.ui.onboarding.mvi.OnboardingMviFeatureFactory
+import javax.inject.Singleton
 
 @Module
 @InstallIn(ViewModelComponent::class)
@@ -34,6 +40,7 @@ class OnboardingModule {
         saveFourthStepValueUseCase: SaveFourthStepValueUseCase,
         getFifthStepValueUseCase: GetFifthStepValueUseCase,
         saveFifthStepValueUseCase: SaveFifthStepValueUseCase,
+        saveManagerDataUseCase: SaveManagerDataUseCase,
     ): OnboardingMviActor = OnboardingMviActor(
         logoutUseCase = logoutUseCase,
         getFirstStepValueUseCase = getFirstStepValueUseCase,
@@ -46,6 +53,7 @@ class OnboardingModule {
         saveFourthStepValueUseCase = saveFourthStepValueUseCase,
         getFifthStepValueUseCase = getFifthStepValueUseCase,
         saveFifthStepValueUseCase = saveFifthStepValueUseCase,
+        saveManagerDataUseCase = saveManagerDataUseCase,
     )
 
     @Provides
