f3a02c5f4e2649762e7f42264701508a7e848d37
DenisGladkih <denis.gladkih.55@yandex.ru>
2024-01-29 15:31:13 +0600

    update RemoteOnboardingRepository

--- pos/app/src/main/java/net/nomia/pos/ui/onboarding/data/repo/RemoteOnboardingRepository.kt
+++ pos/app/src/main/java/net/nomia/pos/ui/onboarding/data/repo/RemoteOnboardingRepository.kt
@@ -3,13 +3,14 @@ package net.nomia.pos.ui.onboarding.data.repo
 import kotlinx.coroutines.flow.Flow
 import net.nomia.common.data.model.Organization
 import net.nomia.main.domain.model.Auth
+import net.nomia.pos.ui.onboarding.model.ManagerData
 import net.nomia.pos.ui.onboarding.model.Resource
 import java.util.UUID
 
 internal interface RemoteOnboardingRepository {
-    fun saveManagerData(auth: Auth): Flow<Resource<Nothing>>
+    fun saveManagerData(auth: Auth?, managerData: ManagerData): Flow<Resource<Nothing>>
 
-    fun getOrganizations(auth: Auth): Flow<Resource<List<Organization>?>>
+    fun getOrganizations(auth: Auth?): Flow<List<Organization>?>
 
-    fun getCatalogId(auth: Auth, organizationId: Organization.ID): Flow<Resource<UUID?>>
+    fun getCatalogId(auth: Auth?, organizationId: Organization.ID): Flow<UUID?>
 }
--- pos/app/src/main/java/net/nomia/pos/ui/onboarding/domain/repo/RemoteOnboardingRepositoryImpl.kt
+++ pos/app/src/main/java/net/nomia/pos/ui/onboarding/domain/repo/RemoteOnboardingRepositoryImpl.kt
@@ -1,28 +1,25 @@
 package net.nomia.pos.ui.onboarding.domain.repo
 
-import com.apollographql.apollo.api.Input
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.catch
-import kotlinx.coroutines.flow.collectLatest
-import kotlinx.coroutines.flow.filterNotNull
 import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.flow.firstOrNull
+import kotlinx.coroutines.flow.flatMapLatest
 import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.flow.mapLatest
-import kotlinx.coroutines.flow.mapNotNull
-import kotlinx.coroutines.flow.transformLatest
 import net.nomia.common.data.model.Organization
 import net.nomia.erp.api.ErpClientService
 import net.nomia.erp.apiFlow
-import net.nomia.erp.mutation.CreateStoreV2Mutation
+import net.nomia.erp.mutation.SaveManagerDataMutation
 import net.nomia.erp.query.GetAllOrganizationsQuery
 import net.nomia.erp.query.GetCatalogQuery
 import net.nomia.erp.schema.type.PageRequestInput
-import net.nomia.erp.schema.type.StoreSurveyStoreService
-import net.nomia.erp.schema.type.StoreSurveyStoreType
-import net.nomia.erp.schema.type.StoreType
 import net.nomia.main.domain.model.Auth
 import net.nomia.main.domain.toDomain
 import net.nomia.pos.ui.onboarding.data.repo.RemoteOnboardingRepository
+import net.nomia.pos.ui.onboarding.domain.toCreateStoreInput
+import net.nomia.pos.ui.onboarding.model.ManagerData
 import net.nomia.pos.ui.onboarding.model.Resource
 import java.util.UUID
 import javax.inject.Inject
@@ -32,67 +29,62 @@ import javax.inject.Singleton
 internal class RemoteOnboardingRepositoryImpl @Inject constructor(
     private val erpClientService: ErpClientService,
 ) : RemoteOnboardingRepository {
+    override fun saveManagerData(
+        auth: Auth?,
+        managerData: ManagerData,
+    ): Flow<Resource<Nothing>> = flow {
 
-    override fun saveManagerData(auth: Auth): Flow<Resource<Nothing>> = flow {
-        emit(Resource.Loading())
+        emit(Resource.Loading)
 
         val organizationId = getOrganizations(auth = auth)
-            .mapNotNull { resource ->
-                resource.data?.firstOrNull()?.id
+            .map { organizations ->
+                organizations?.firstOrNull()?.id ?: error("organization id is null")
+            }
+            .catch { e ->
+                emit(Resource.Error(e.message))
             }
             .first()
 
         val catalogId = getCatalogId(auth = auth, organizationId = organizationId)
-            .mapNotNull { resource ->
-                resource.data
+            .map { uuid ->
+                uuid ?: error("catalog id is null")
+            }
+            .catch { e ->
+                emit(Resource.Error(e.message))
             }
             .first()
 
         val client = erpClientService.principalClient(
-            token = auth.accessToken,
-            refreshToken = auth.refreshToken,
+            token = auth?.accessToken,
+            refreshToken = auth?.refreshToken,
             organizationId = organizationId,
-        ).catch {
-            emit(Resource.Error(message = it.message))
-        }
-            .filterNotNull()
-            .first()
+        )
+            .firstOrNull()
+            ?: error("client is null")
 
         client.mutate(
-            CreateStoreV2Mutation(
-                organizationId = organizationId.value,
-                catalogId = catalogId,
-                type = StoreType.RESTAURANT,
-                currency = "RUB",
-                name = "тестовое имя",
-                phone = Input.absent(),
-                organizationName = Input.optional("тестовая организация"),
-                address = Input.optional("тестовый адрес"),
-                previousErp = Input.absent(),
-                storeTypes = listOf(
-                    StoreSurveyStoreType.BAR,
-                    StoreSurveyStoreType.CAFE
+            SaveManagerDataMutation(
+                input = managerData.toCreateStoreInput(
+                    organizationId = organizationId.value,
+                    catalogId = catalogId,
                 ),
-                totalArea = Input.optional(1),
-                publicArea = Input.optional(2),
-                kitchenArea = Input.optional(3),
-                seatingCapacity = Input.optional(3),
-                storeServices = listOf(StoreSurveyStoreService.DELIVERY)
-            )
+            ),
         )
-            .apiFlow()
-            .catch { e ->
+            ?.apiFlow()
+            ?.catch { e ->
                 emit(Resource.Error(e.message))
             }
-
-        emit(Resource.Success())
+            ?.collect {
+                emit(Resource.Success())
+            }
     }
 
-    override fun getOrganizations(auth: Auth): Flow<Resource<List<Organization>?>> =
-        erpClientService.principalClient(
-            token = auth.accessToken,
-            refreshToken = auth.refreshToken,
-        ).transformLatest { client ->
+    override fun getOrganizations(auth: Auth?): Flow<List<Organization>?> = erpClientService
+        .principalClient(
+            token = auth?.accessToken,
+            refreshToken = auth?.refreshToken,
+        )
+        .flatMapLatest { client ->
             client.query(
                 GetAllOrganizationsQuery(
                     PageRequestInput(
@@ -102,38 +94,29 @@ internal class RemoteOnboardingRepositoryImpl @Inject constructor(
                 )
             )
                 .apiFlow()
-                .catch { e ->
-                    emit(Resource.Error(message = e.message))
-                }
                 .mapLatest { response ->
                     response.data
                         ?.allOrganizationsPageable
                         ?.content
                         ?.map { content -> content.fragments.organizationFragment.toDomain() }
-                }
-                .filterNotNull()
-                .collectLatest { listOrganizations ->
-                    emit(Resource.Success(listOrganizations))
+
                 }
         }
 
-
     override fun getCatalogId(
-        auth: Auth,
-        organizationId: Organization.ID
-    ): Flow<Resource<UUID?>> = erpClientService.principalClient(
-        token = auth.accessToken,
-        refreshToken = auth.refreshToken,
-        organizationId = organizationId,
-    ).transformLatest { client ->
-        client.query(GetCatalogQuery())
-            .apiFlow()
-            .catch { e ->
-                emit(Resource.Error(message = e.message))
-            }
-            .collectLatest { response ->
-                val data = response.data?.catalogByOrganization?.id
-                emit(Resource.Success(data = data))
-            }
-    }
+        auth: Auth?,
+        organizationId: Organization.ID,
+    ): Flow<UUID?> = erpClientService
+        .principalClient(
+            token = auth?.accessToken,
+            refreshToken = auth?.refreshToken,
+            organizationId = organizationId,
+        )
+        .flatMapLatest { client ->
+            client.query(GetCatalogQuery())
+                .apiFlow()
+                .mapLatest { response ->
+                    response.data?.catalogByOrganization?.id
+                }
+        }
 }
